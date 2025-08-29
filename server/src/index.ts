332cf42fed30486a9edee541fbdf051887e35308
Chirag Bhatia <chirag.bhatia.14567@gmail.com>
2025-08-29 22:33:14 +0530

    Initialize server with TypeScript, Express, and CORS support
    - Created package.json with necessary dependencies and scripts for development and production.
    - Implemented app.ts to set up Express server with CORS configuration.
    - Added index.ts to start the server and load environment variables from .env file.
    - Configured TypeScript with tsconfig.json for compilation settings.

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config({path : "./.env"});
+const PORT = process.env.PORT
+
+const startServer = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log("Server is running on port", PORT);
+        })
+    } catch(error) {
+        console.error("Error starting the server:", error);
+    }
+}
+startServer();
