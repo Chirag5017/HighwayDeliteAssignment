a0a252dd8036fc362cedd224310b93d66fe8a583
Chirag5017 <147335611+Chirag5017@users.noreply.github.com>
2025-08-31 13:46:46 +0000

    normal fix for production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard';
 import { AuthProvider, useAuth } from './context/AuthContext';
